e15775ac6c0c0afed706e2964bb6a87324626867
MizutaniSubaru <MizutaniSubaru@outlook.com>
2026-04-19 23:14:36 +0800

    chore: remove old UI components and reset entry point for V1 rewrite

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { AppRouter } from "./app/router";
-import "./styles/app.css";
+import { App } from "./App";
+import "./styles/variables.css";
 
 const rootElement = document.getElementById("root");
 
@@ -11,7 +11,7 @@ if (!rootElement) {
 
 createRoot(rootElement).render(
   <StrictMode>
-    <AppRouter />
+    <App />
   </StrictMode>,
 );
 
